5cd6bd5d0a583e8a31b6a74959e9cfcd13536cef
John Corr <johnnycorr@gmail.com>
2020-04-21 01:02:11 +1000

    Added spotify auth link, began building component for link

--- src/App.js
+++ src/App.js
@@ -2,6 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function Auth(props) {
+  const endpoint = this.props.endpoint;
+  const client_id = this.props.client_id;
+  const redirect_uri = this.props.redirect_uri;
+  const scope = this.props.scope;
+
+  return (
+    <a href="#"></a>
+  );
+}
+
 function App() {
   return (
     <div className="App">
@@ -19,6 +30,15 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <main>
+        <a
+        className="auth-login"
+        href="https://accounts.spotify.com/authorize?client_id=81cee7973b1a4951a4af313823b614df&redirect_uri=http:%2F%2Flocalhost%3A3000&scope=user-read-private%20user-read-email&response_type=token&state=123"
+        >
+          Spotify authorisation
+        </a>
+      </main>
     </div>
   );
 }
